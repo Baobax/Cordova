69d41ea2fff3c515e1992d2484b6eaef3cd38427
Baptiste <baptiste.henot@free.fr>
2018-03-05 11:56:35 +0100

    Première version sans le random de l'animation qui affiche deux cartes

--- www/js/Controller.js
+++ www/js/Controller.js
@@ -5,24 +5,27 @@ var Controller = function() {
         initialize: function() {
             self = this;
 
-            $(".card").on("click", self.clickAnimateFlip);
-            $(".check_pref").on("click", self.clickPref);
+            $(".top_card").on("click", self.clickAnimateFlip);
             //$(".link").on("click", self.changePage);
         },
 
         clickAnimateFlip: function () {
+            //alert(Math.floor((Math.random() * 54) + 1));
             if(forward) {
-                $(".card").attr("class", "card animate");
+                $(".top_card").attr("class", "top_card animate_top_card");
+                $(".card").html("<img class='front' src='img/card_front.png'><br>"
+                    + "<img class='back' src='img/card_back.png'>");
+                $(".card").attr("class", "card animate_card");
+                $(".card .back").unbind();
+                $(".card .back").on("click", self.clickAnimateFlip);
                 forward = false;
             }
             else {
+                $(".top_card").attr("class", "top_card");
                 $(".card").attr("class", "card");
+                //$(".card").empty();
                 forward = true;
             }
-        },
-
-        clickPref: function (e) {
-            e.stopImmediatePropagation();
         },
 
         changePage: function() {
