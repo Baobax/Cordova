75227b840806b310f14ae0f0711fb25ce321ccf2
Baptiste <baptiste.henot@free.fr>
2018-03-08 15:07:31 +0100

    Changement de la syntaxe du javascript et regroupement des deux fichiers javascript

--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,80 @@
-var controller;
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+var pause = false;
+var forward = true;
 
-    onDeviceReady: function() {
-        controller = new Controller();
-    },
-};
+//Cordova events------------------------------------------------
+function initialize() {
+    document.addEventListener('deviceready', onDeviceReady, false);
+    document.addEventListener("pause", onPause, false);
+    document.addEventListener("resume", onResume, false);
+}
 
-app.initialize();
+function onDeviceReady() {
+    $(".top_card").on("click", clickAnimateFlipTopCard);
+    $(".card").on("click", clickAnimateFlipCard);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+async function backgroundTask() {
+    pause = true;
+    var i = 0, card = 0, carteTiree = false;
+
+    while(pause && !carteTiree) {
+        console.log(i + " ème tour de boucle");
+        card = Math.floor((Math.random() * 10) + 1);
+        if(card == 3) {
+            console.log("Carte tirée");
+            carteTiree = true;
+        }
+        i++;
+        await sleep(2000);
+    }   
+}
+
+function onPause() {
+    backgroundTask();
+}
+
+function onResume() {
+    pause = false;
+    console.log("Resume");
+}
+
+
+//My events------------------------------------------------
+function clickAnimateFlipTopCard() {
+    var numCard = Math.floor((Math.random() * 2) + 1);
+    
+    //Changing front card src
+    $(".card .front .small").attr("srcset", "img/cards/" + numCard + "_card_front.png");
+    $(".card .front .medium").attr("srcset", "img/cards/" + numCard + "_card_front.png");
+    $(".card .front img").attr("src", "img/cards/" + numCard + "_card_front.png");
+
+    //Changing back card src
+    $(".card .back .small").attr("srcset", "img/cards/" + numCard + "_card_back.png");
+    $(".card .back .medium").attr("srcset", "img/cards/" + numCard + "_card_back.png");
+    $(".card .back img").attr("src", "img/cards/" + numCard + "_card_back.png");
+
+    $(".card")
+    .transition({scale:1, duration: 200});
+}
+
+function clickAnimateFlipCard() {
+   if(forward) {
+       $(".card").transition({rotateY:'180deg', duration: 200});
+       forward = false;
+   }
+   else {
+       $(".card")
+       .transition({rotateY:'0deg', duration: 20})
+       .transition({y:0, duration: 200})
+       .transition({scale:0, duration: 200});
+
+       forward = true;
+   }
+}
+
+initialize();
