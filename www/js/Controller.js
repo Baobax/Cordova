2443984f0cb251562614210c0d140fc5a872a31f
Baptiste <baptiste.henot@free.fr>
2018-03-08 12:05:55 +0100

    Travail sur le côté responsive, la carte s'affiche en plein écran

--- www/js/Controller.js
+++ www/js/Controller.js
@@ -11,42 +11,47 @@ var Controller = function() {
 
         clickAnimateFlipTopCard: function () {
             var numCard = Math.floor((Math.random() * 2) + 1);
-            var cardHeight = ($(".top_card img").height() / 1.6) + ($(".paquet").height() / 1.6) + 10;
+            //var transformHeight = $(".top_card img").height() + $(".paquet").height() + 10;
 
-
-            $(".top_card")
+            /*$(".top_card")
                 .transition({y:-50, duration: 200})
-                .transition({y:0, duration: 200});
+                .transition({y:0, duration: 200});*/
 
-            $(".card .front").attr("src", "img/cards/" + numCard + "_card_front.png");
-            $(".card .back").attr("src", "img/cards/" + numCard + "_card_back.png");
+            /*$(".card").html("<img class='front' src='img/cards/" + numCard + "_card_front.png'>"
+                + "<img class='back' src='img/cards/" + numCard + "_card_back.png'>");*/
 
-            $(".res").html(cardHeight);
+            //Changing front card src
+            $(".card .front .small").attr("srcset", "img/cards/" + numCard + "_card_front.png");
+            $(".card .front .medium").attr("srcset", "img/cards/" + numCard + "_card_front.png");
+            $(".card .front img").attr("src", "img/cards/" + numCard + "_card_front.png");
+
+            //Changing back card src
+            $(".card .back .small").attr("srcset", "img/cards/" + numCard + "_card_back.png");
+            $(".card .back .medium").attr("srcset", "img/cards/" + numCard + "_card_back.png");
+            $(".card .back img").attr("src", "img/cards/" + numCard + "_card_back.png");
 
             $(".card")
-                .transition({scale:1.6, duration: 200})
-                .transition({y:-cardHeight, duration: 200});
+                .transition({scale:1, duration: 200});
+               // .transition({y:-transformHeight, duration: 200});
         },
 
         clickAnimateFlipCard: function () {
-            if(forward) {
-                $(".card").transition({perspective:'1000px', rotateY:'180deg', duration: 200});
-                
-                forward = false;
-            }
-            else {
-                $(".card")
-                    .transition({perspective:'1000px', rotateY:'0deg', duration: 20})
-                    .transition({y:0, duration: 200})
-                    .transition({scale:0, duration: 200});
+           if(forward) {
+               $(".card").transition({rotateY:'180deg', duration: 200});
+               forward = false;
+           }
+           else {
+               $(".card")
+                   .transition({rotateY:'0deg', duration: 20})
+                   .transition({y:0, duration: 200})
+                   .transition({scale:0, duration: 200});
 
-                $(".top_card")
-                    .transition({y:-50, duration: 300})
-                    .transition({y:0, duration: 200});
-
-                forward = true;
-            }
-        },
+               /*$(".top_card")
+                   .transition({y:-50, duration: 300})
+                   .transition({y:0, duration: 200});*/
+               forward = true;
+           }
+       },
     }
     controller.initialize();
     return controller;
